a875d188f90c9e229767c11c9974a2c9dfde20c4
TheGreenBit <theredblock12@gmail.com>
2025-05-07 14:23:01 +0200

    Main

---
+++ main.lua
@@ -0,0 +1,9 @@
+--[[ Libraries ]]
+_G.love = require("love")
+
+_G.Utils = require("lib/Utils")
+_G.ConfigController = require("src/ConfigController")
+
+_G.ConnectLib = require("lib/ConnectLib")
+_G.Update = require("lib/Update")
+_G.DrawObjects = require("lib/DrawObjects")
